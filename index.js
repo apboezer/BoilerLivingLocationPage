faabb631572c74e4db0d0853d6589f43aefbc44d
mtwind <112357691+mtwind@users.noreply.github.com>
2023-01-21 15:53:53 -0500

    2 files modified

--- index.js
+++ index.js
@@ -15,3 +15,18 @@ var imgSource = 'https://i.ibb.co/r7Sw159/0-Stars.jpg'; //placeholder
 var pageImg = document.getElementById('location-image');
 pageImg.src = imgSource; 
 //appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+  var locationScore = 0;
+  var qualityScore = 0;
+  var amenitiesScore = 0;
+  var communityScore = 0;
+
+  const submit = document.getElementById('submit-button');
+
+  submit.addEventListener('click', () => {
+    locationScore = document.getElementById('location-score').value;
+    qualityScore = document.getElementById('quality-score').value;
+    amenitiesScore = document.getElementById('amenities-score').value;
+    communityScore = document.getElementById('community-score').value;
+    console.log(locationScore + " " + qualityScore + " " + amenitiesScore + " " + communityScore);
+  });
