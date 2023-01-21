1206dbfec286dae0a06426183f0bd165adc287c8
apboezer <111909744+apboezer@users.noreply.github.com>
2023-01-21 15:06:50 -0500

    2 files modified

--- index.js
+++ index.js
@@ -2,5 +2,12 @@
 import './style.css';
 
 // Write Javascript code!
+
+//renaming to specific location name
+const locName = 'placeholder';
 const appDiv = document.getElementById('app');
+var nameLabels = document.getElementsByClassName('location-name');
+[].forEach.call(nameLabels, function (nameLabel) {
+  nameLabel.innerHTML = locName;
+});
 //appDiv.innerHTML = `<h1>JS Starter</h1>`;
