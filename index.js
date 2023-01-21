b5e975cdc9b4f3cefe0c3416b406984734c74e85
mtwind <112357691+mtwind@users.noreply.github.com>
2023-01-21 17:23:47 -0500

    2 files modified

--- index.js
+++ index.js
@@ -25,16 +25,20 @@ pageImg.src = imgSource;
   var qualityScore = 0;
   var amenitiesScore = 0;
   var communityScore = 0;
+  var average = 0;
+  var reviewMessage = "";
 
   const submit = document.getElementById('submit-button');
 
   submit.addEventListener('click', () => {
+    reviewMessage = document.getElementById('review-message').value;
+
     locationScore = +document.getElementById('location-score').value;
     qualityScore = +document.getElementById('quality-score').value;
     amenitiesScore = +document.getElementById('amenities-score').value;
     communityScore = +document.getElementById('community-score').value;
     average = (locationScore + qualityScore +  amenitiesScore + communityScore) / 4;
 
-    console.log(locationScore + " " + qualityScore + " " + amenitiesScore + " " + communityScore + " " + average);
+    console.log(locationScore + " " + qualityScore + " " + amenitiesScore + " " + communityScore + " " + average + ". " + reviewMessage);
   });
 
